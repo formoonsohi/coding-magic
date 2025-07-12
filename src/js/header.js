fafa4a0eb872832f2c0164ad73666bbdf47765b8
lizkisssss <lizasalamakha2010@gmail.com>
2025-07-12 12:47:58 +0300

    icons-and-js

--- src/js/header.js
+++ src/js/header.js
@@ -1,12 +1,32 @@
+import sunImage from "../img/themeLight.png";
+import moonImage from "../img/themeDark.png";
+import arrowDownDark from "../img/arrow-down.svg";
+import arrowDownLight from "../img/arrow-down-light.svg";
+import codingMagicLogoDarkTheme from "../img/coding-magic-light.svg"; 
+import codingMagicLogoLightTheme from "../img/coding-magic.svg"; 
+
+const toggleButton = document.getElementById('toggle-theme');
+const imgElement = toggleButton.firstElementChild;
+const arrowIcon = document.getElementById('arrow-icon');
+const codingMagicLogo = document.getElementById('coding-light');
+
 let darkMode = false;
-document.getElementById('toggle-theme').addEventListener('click', () => {
-darkMode = !darkMode;
+
+toggleButton.addEventListener('click', () => {
+    darkMode = !darkMode;
 
     if (darkMode) {
-    document.documentElement.style.setProperty('--main-text-color', '#000');
-    document.documentElement.style.setProperty('--main-bg-color', '#fff');
-      } else {
-    document.documentElement.style.setProperty('--main-bg-color', '#fff');
-    document.documentElement.style.setProperty('--main-text-color', '#000');
-      }
-    });
+        imgElement.src = moonImage;
+        document.documentElement.style.setProperty('--main-text-color', '#fff');
+        document.documentElement.style.setProperty('--main-bg-color', '#000');
+        arrowIcon.src = arrowDownDark;
+        codingMagicLogo.src = codingMagicLogoDarkTheme;
+
+    } else {
+        imgElement.src = sunImage;
+        document.documentElement.style.setProperty('--main-bg-color', '#fff');
+        document.documentElement.style.setProperty('--main-text-color', '#000');
+        arrowIcon.src = arrowDownLight;
+        codingMagicLogo.src = codingMagicLogoLightTheme;
+    }
+});
